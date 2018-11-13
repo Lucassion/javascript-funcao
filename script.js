3846f2af14c5fdd27f787911b419a39cafd57e32
Lucassion <lucasinventor@gmail.com>
2018-11-12 22:00:28 -0200

    Javascript função

---
+++ script.js
@@ -0,0 +1,45 @@
+// let valor1 = 1;
+// let valor2 = 2;
+
+// console.log(valor1 + valor2);
+
+/*
+    Anotações
+
+Assinatura da função
+Entre parenteses são os parametros da função
+Função que não utiliza variaveis de fora: função pura
+Utiliza de fora: Não pura
+
+Função anônima: Não possui um nome.
+
+Variavel de escolpo global: O que existe somente, fora de funções.
+Escolpo de bloco: dentro de "if/else/for".
+Escolpo de função: dentro de função.
+
+****** "var" quebra o código, altera a ordem das funções ******
+****** "var" NÃO respeita escolpos. *******
+
+****** "return" quebra o código ******
+
+******O Javascript le o código previamente o coloca todos os "var" e "function" no topo do código, por isso ele quebra.*****
+
+.subsbtring(valor que você quer) = retira e expõem depois dele
+
+*/
+console.log(valor);
+var valor = 2;
+
+
+let dividir = function (a,b){
+    return a/b;
+}
+
+function somar(a, b){
+    return soma;
+}
+
+function multiplicar (a, b){
+    return a* b;
+}
+
